e638ba43daa57d143c8700c7e7d3426c3fd01d00
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2026-03-23 15:10:44 +1100

    feat: create the project constants

---
+++ lib/constants/app.dart
@@ -0,0 +1,42 @@
+/// App-wide constants for Sanctum.
+//
+// Time-stamp: <>
+//
+/// Copyright (C) 2025, Cyrill Adrian Wicaksono
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://opensource.org/license/gpl-3-0
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
+///
+/// Authors: Cyrill Adrian Wicaksono
+
+library;
+
+/// The display title of the application.
+
+const String kAppTitle = 'SANCTUM';
+
+/// The directory name used to store app data inside the Solid Pod.
+
+const String kAppDirectory = 'sanctum';
+
+/// The default Solid Pod server URL used on the login screen.
+
+const String kDefaultServerUrl = 'https://solidcommunity.au';
+
+/// The current version string of the application.
+
+const String kAppVersion = '1.0.0';
