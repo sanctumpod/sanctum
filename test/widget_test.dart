0eab8f2e255050cb882e324cb9c1148fd0fe8818
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2026-03-23 17:12:37 +1100

    fix: replace the autogenerated smoke test references with a minimal Sanctum smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,42 @@
-// This is a basic Flutter widget test.
+/// Smoke test verifying the Sanctum widget tree builds without error.
 //
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Time-stamp: <>
+//
+/// Copyright (C) 2025, Cyrill Adrian Wicaksono
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://opensource.org/license/gpl-3-0
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
+///
+/// Authors: Cyrill Adrian Wicaksono
 
-import 'package:flutter/material.dart';
+library;
+
+// Group 1: Flutter/Dart SDK imports.
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sanctum/main.dart';
+// Group 2: Third-party package imports.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Group 3: Local package imports.
+import 'package:sanctum/sanctum.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Sanctum widget builds without error', (WidgetTester tester) async {
+    // Pump the root widget inside a ProviderScope as required by Riverpod.
+    await tester.pumpWidget(const ProviderScope(child: Sanctum()));
   });
 }
