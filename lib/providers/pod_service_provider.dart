26d49e34340ea97bf06889f099cfbe92a0fddeda
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2026-04-07 15:25:56 +1000

    feat: add podServiceProvider and TransactionListNotifier

---
+++ lib/providers/pod_service_provider.dart
@@ -0,0 +1,38 @@
+/// Riverpod provider that exposes the singleton PodService.
+//
+// Time-stamp: <>
+//
+/// Copyright (C) 2025, Cyrill Adrian Wicaksono
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://opensource.org/license/gpl-3-0
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
+///
+/// Authors: Cyrill Adrian Wicaksono
+
+library;
+
+// Group 2: Third-party package imports.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Group 3: Local package imports.
+import 'package:sanctum/services/pod_service.dart';
+
+/// Exposes the application-wide [PodService] instance.
+///
+/// All data providers read from this provider to access Pod storage.
+/// Widgets must never instantiate [PodService] directly.
+final podServiceProvider = Provider<PodService>((ref) => PodService());
---
+++ lib/providers/transaction_providers.dart
@@ -0,0 +1,60 @@
+/// Riverpod providers for Transaction state management.
+//
+// Time-stamp: <>
+//
+/// Copyright (C) 2025, Cyrill Adrian Wicaksono
+///
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://opensource.org/license/gpl-3-0
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
+///
+/// Authors: Cyrill Adrian Wicaksono
+
+library;
+
+// Group 2: Third-party package imports.
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Group 3: Local package imports.
+import 'package:sanctum/models/transaction.dart';
+import 'package:sanctum/providers/pod_service_provider.dart';
+
+/// Manages the list of transactions loaded from the user's Pod.
+///
+/// Invalidates itself after every mutation so the UI stays in sync.
+class TransactionListNotifier extends AsyncNotifier<List<Transaction>> {
+  @override
+  Future<List<Transaction>> build() =>
+      ref.read(podServiceProvider).loadAllTransactions();
+
+  /// Saves [tx] to the Pod and refreshes the transaction list.
+  Future<void> add(Transaction tx) async {
+    await ref.read(podServiceProvider).saveTransaction(tx);
+    ref.invalidateSelf();
+  }
+
+  /// Deletes the transaction with [id] from the Pod and refreshes the list.
+  Future<void> delete(String id) async {
+    await ref.read(podServiceProvider).deleteTransaction(id);
+    ref.invalidateSelf();
+  }
+}
+
+/// Provides the async list of all transactions for the current user.
+final transactionListProvider =
+    AsyncNotifierProvider<TransactionListNotifier, List<Transaction>>(
+  TransactionListNotifier.new,
+);
